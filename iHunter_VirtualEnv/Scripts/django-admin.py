2daa398c051d05d49fea41d77e1c6eb817ff90f9
Sandeep Kumar <kumarsandeep91@gmail.com>
2017-05-17 04:37:14 +0530

    removing virtualenv setup files from versioning..

--- iHunter_VirtualEnv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Russet\Projects\opensource\Russet.iHunter.Model\iHunter\iHunter_VirtualEnv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
